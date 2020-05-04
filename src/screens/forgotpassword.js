36d775c3c209117740568a0a93614ba7a1517696
Vatsalsoni13 <sonivatsal2001@gmail.com>
2020-05-04 17:03:00 +0530

    Image Picker and Password Reset

--- src/screens/forgotpassword.js
+++ src/screens/forgotpassword.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   TouchableOpacity,
   Alert,
+  Dimensions,
 } from 'react-native';
 import auth from '@react-native-firebase/auth';
 export default class ForgotPassword extends React.Component {
@@ -35,13 +36,22 @@ export default class ForgotPassword extends React.Component {
       <View style={styles.container}>
         {/* <Text>Password</Text> */}
         <TextInput
-          placeholder="Enter emailId"
+          style={{
+            padding: 10,
+            borderWidth: 1,
+            width: Dimensions.get('screen').width - 30,
+            borderRadius: 10,
+            margin: 20,
+          }}
+          placeholder="Enter email-Id"
           value={this.state.email}
           keyboardType="email-address"
           onChangeText={(email) => this.setState({email})}
         />
-        <TouchableOpacity onPress={() => this.forgotPassword(this.state.email)}>
-          <Text>Change Password</Text>
+        <TouchableOpacity
+          style={{padding: 20, backgroundColor: '#930b0d', borderRadius: 10}}
+          onPress={() => this.forgotPassword(this.state.email)}>
+          <Text style={{color: '#FFF'}}>Change Password</Text>
         </TouchableOpacity>
       </View>
     );
